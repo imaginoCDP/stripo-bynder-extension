068bc597f7f773c42c4763cd869328a9a908a608
enaitkaci <elyes.nait-kaci@imagino.com>
2023-01-16 16:52:49 +0100

    Case of none supported format

--- src/js/modal_window.js
+++ src/js/modal_window.js
@@ -4,6 +4,10 @@ const TYPE_VIDEO = "VIDEO";
 const TAG_NAME_IMAGE = ".bynderImage";
 const TAG_NAME_VIDEO = ".bynderVideo";
 const TAG_NAME_VIDEO_LINK = ".bynderVideoLink";
+const TAG_NAME_VIDEO_LINK_IMAGE = ".bynderVideoLinkImage";
+const TAG_NAME_IMAGE_NOT_SUPPORTED = "bynderImageNotSupported";
+const TAG_NAME_IMAGE_LINK_NOT_SUPPORTED = "bynderImageLinkNotSupported";
+
 const TAG_NAME_DEFAULT = ".bynderDefault";
 
 export function openBynder(portalURL, token, block, locale) {
@@ -25,26 +29,41 @@ export function openBynder(portalURL, token, block, locale) {
     assetTypes: ["image", "video"], // Take off document type since no way to display it properly
     onSuccess: (assets, additionalInfo) => {
       const asset = assets?.[0];
-      const { type, derivatives, previewUrls } = asset;
+      const { type, derivatives, previewUrls, extensions } = asset;
 
       block.find(TAG_NAME_DEFAULT).css("display", "none");
       switch (type) {
         case TYPE_IMAGE:
-          // NOTE: images with .tif extension might not work proprely
-          // since all link within this type of asset are download links
-          block.find(TAG_NAME_VIDEO_LINK).css("display", "none");
-          block
-            .find(TAG_NAME_IMAGE)
-            .css("display", "inline")
-            .attr("src", additionalInfo?.selectedFile?.url);
+          // NOTE: images with .tif .eps extension might not work proprely
+          // since most browsers does not support it
+          if (extensions.includes("eps") || extensions.includes("tif")) {
+            block.find(TAG_NAME_VIDEO).css("display", "none");
+            block.find(TAG_NAME_IMAGE).css("display", "none");
+            block.find(TAG_NAME_IMAGE_NOT_SUPPORTED).css("display", "inline");
+            block
+              .find(TAG_NAME_IMAGE_LINK_NOT_SUPPORTED)
+              .css("display", "inline")
+              .attr("href", additionalInfo?.selectedFile?.url);
+          } else {
+            block.find(TAG_NAME_VIDEO).css("display", "none");
+            block.find(TAG_NAME_IMAGE_NOT_SUPPORTED).css("display", "none");
+            block
+              .find(TAG_NAME_IMAGE)
+              .css("display", "inline")
+              .attr("src", additionalInfo?.selectedFile?.url);
+          }
           break;
         case TYPE_VIDEO:
           block.find(TAG_NAME_IMAGE).css("display", "none");
+          block.find(TAG_NAME_IMAGE_NOT_SUPPORTED).css("display", "none");
           block
-            .find(TAG_NAME_VIDEO_LINK)
+            .find(TAG_NAME_VIDEO)
             .css("display", "inline")
-            .attr("href", previewUrls[0].url);
-          block.find(TAG_NAME_VIDEO).attr("src", derivatives?.webImage);
+            .attr("src", previewUrls[0].url);
+          block.find(TAG_NAME_VIDEO_LINK).attr("href", previewUrls[0].url);
+          block
+            .find(TAG_NAME_VIDEO_LINK_IMAGE)
+            .attr("src", derivatives?.webImage);
           break;
       }
     },
